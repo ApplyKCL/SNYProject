153de1a511e7926773512cb6584558f8143f691a
Arthur-ca <ch243852@gmail.com>
2023-01-19 20:49:58 -0400

    fixed transfer function between windows - Jiahao

--- UI_design/LoginUI_Design/main.py
+++ UI_design/LoginUI_Design/main.py
@@ -1,6 +1,5 @@
 from PyQt5.QtWidgets import QApplication, QMainWindow
 import sys
-import resource_rc
 from login import *
 from Admin_window import *
 
@@ -15,9 +14,8 @@ class MyWindow(QMainWindow, Ui_Login_Window):
         account = self.UserID.text()
         password = self.Password.text()
         if account == "Jiahao" and password == "950321":
-            otherwindow = Administrator_Window()
-            otherwindow.show()
-            self.close()
+            self.hide()
+            admin_window.show()
         else:
             pass
         
@@ -33,11 +31,22 @@ class MyWindow(QMainWindow, Ui_Login_Window):
 class Administrator_Window(QMainWindow, Ui_Administrator_window):
     def __init__(self):
         super().__init__()
+        self.setWindowFlag(QtCore.Qt.FramelessWindowHint)
         self.setupUi(self)
+        
+    def mousePressEvent(self, event):                                 # +
+        self.dragPos = event.globalPos()
+        
+    def mouseMoveEvent(self, event):                                  # !!!
+        if event.buttons() == QtCore.Qt.LeftButton:
+            self.move(self.pos() + event.globalPos() - self.dragPos)
+            self.dragPos = event.globalPos()
+            event.accept()
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     myWindow = MyWindow()
+    admin_window = Administrator_Window()
     myWindow.show()
     sys.exit(app.exec_())
