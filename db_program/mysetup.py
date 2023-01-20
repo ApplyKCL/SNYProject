b162667f7f390d17007fdc209e369d32841ed88c
Shaonan Hu <shaonan1901@163.com>
2023-01-19 21:01:24 -0400

    Add New File

---
+++ db_program/mysetup.py
@@ -0,0 +1,7 @@
+import os
+# from barcode import EAN13
+os.system('date')
+print("Configure MySQL Python Package")
+os.system('pip install mysql-connector-python')
+os.system('pip install python-barcode')
+
