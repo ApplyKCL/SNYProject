2cc42bda31bc1fe750199d026c91242837b0099f
Arthur-ca <ch243852@gmail.com>
2023-03-05 01:35:39 -0400

    Updates on date

--- UI_design/LoginUI_Design/main.py
+++ UI_design/LoginUI_Design/main.py
@@ -3,7 +3,7 @@ import sys
 from login import *
 # from Admin_Window_1 import *
 from Normal_user import *
-from ppt import *
+from Instruction_Window import *
 
 ### Login Main Window ###
 class MyWindow(QMainWindow, Ui_Login_Window):
@@ -23,6 +23,15 @@ class MyWindow(QMainWindow, Ui_Login_Window):
             self.hide()
             user_window.showFullScreen()
             pass
+        
+    def mousePressEvent(self, event):                                 # +
+        self.dragPos = event.globalPos()
+        
+    def mouseMoveEvent(self, event):                                  # !!!
+        if event.buttons() == QtCore.Qt.LeftButton:
+            self.move(self.pos() + event.globalPos() - self.dragPos)
+            self.dragPos = event.globalPos()
+            event.accept()
         
 # class Administrator_Window(QMainWindow, Ui_Admin_WIndow):
 #     def __init__(self):
@@ -55,12 +64,16 @@ class User_Window(QMainWindow, Ui_User_WIndow):
         self.hide()
         ppt.showFullScreen()
             
-class PPT_Window(QMainWindow, Ui_MainWindow2):
+class Instruction_Window(QMainWindow, Ui_InstructionWindow):
     def __init__(self):
         super().__init__()
         self.setWindowFlag(QtCore.Qt.FramelessWindowHint)
         self.setupUi(self)
-        self.stackedWidget.setCurrentWidget(self.home)
+        self.stackedWidget.setCurrentWidget(self.HomePage)
+        self.HomePage_Next.clicked.connect(lambda: self.stackedWidget.setCurrentIndex(1))
+        self.Mount_Piezo_Wafer_1_Back.clicked.connect(lambda: self.stackedWidget.setCurrentIndex(0))
+        self.Mount_Piezo_Wafer_1_Next.clicked.connect(lambda: self.stackedWidget.setCurrentIndex(2))
+        self.Mount_Piezo_Wafer_2_Back.clicked.connect(lambda: self.stackedWidget.setCurrentIndex(1))
         
     def keyPressEvent(self, event):
         if event.key() == QtCore.Qt.Key_Escape:
@@ -72,6 +85,6 @@ if __name__ == '__main__':
     myWindow = MyWindow()
     # admin_window = Administrator_Window()
     user_window = User_Window()
-    ppt = PPT_Window()
+    ppt = Instruction_Window()
     myWindow.show()
     sys.exit(app.exec_())
