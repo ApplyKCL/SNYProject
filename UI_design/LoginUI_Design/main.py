b0c87244e0a3606de46a4c78f6a8affd6c77c351
Arthur-ca <ch243852@gmail.com>
2023-03-04 22:05:23 -0400

    Update Full Screen & "ESC" key to exit these functionalities.

--- UI_design/LoginUI_Design/main.py
+++ UI_design/LoginUI_Design/main.py
@@ -1,10 +1,11 @@
 from PyQt5.QtWidgets import QApplication, QMainWindow
 import sys
 from login import *
-from Admin_Window import *
+# from Admin_Window_1 import *
 from Normal_user import *
 from ppt import *
 
+### Login Main Window ###
 class MyWindow(QMainWindow, Ui_Login_Window):
     def __init__(self):
         super().__init__()
@@ -17,40 +18,31 @@ class MyWindow(QMainWindow, Ui_Login_Window):
         password = self.Password.text()
         if account == "Jiahao" and password == "950321":
             self.hide()
-            admin_window.show()
+            # admin_window.show()
         else:
             self.hide()
-            user_window.show()
+            user_window.showFullScreen()
             pass
         
-    def mousePressEvent(self, event):                                 # +
-        self.dragPos = event.globalPos()
-        
-    def mouseMoveEvent(self, event):                                  # !!!
-        if event.buttons() == QtCore.Qt.LeftButton:
-            self.move(self.pos() + event.globalPos() - self.dragPos)
-            self.dragPos = event.globalPos()
-            event.accept()
-        
-class Administrator_Window(QMainWindow, Ui_Admin_WIndow):
-    def __init__(self):
-        super().__init__()
-        self.setWindowFlag(QtCore.Qt.FramelessWindowHint)
-        self.setupUi(self)
-        self.close_pushButton.clicked.connect(self.close_event)
+# class Administrator_Window(QMainWindow, Ui_Admin_WIndow):
+#     def __init__(self):
+#         super().__init__()
+#         self.setWindowFlag(QtCore.Qt.FramelessWindowHint)
+#         self.setupUi(self)
+#         self.close_pushButton.clicked.connect(self.close_event)
     
-    def close_event(self):
-        self.hide()
-        myWindow.show()
+#     def close_event(self):
+#         self.hide()
+#         myWindow.show()
         
-    def mousePressEvent(self, event):                                 # +
-        self.dragPos = event.globalPos()
+#     def mousePressEvent(self, event):                                 # +
+#         self.dragPos = event.globalPos()
         
-    def mouseMoveEvent(self, event):                                  # !!!
-        if event.buttons() == QtCore.Qt.LeftButton:
-            self.move(self.pos() + event.globalPos() - self.dragPos)
-            self.dragPos = event.globalPos()
-            event.accept()
+#     def mouseMoveEvent(self, event):                                  # !!!
+#         if event.buttons() == QtCore.Qt.LeftButton:
+#             self.move(self.pos() + event.globalPos() - self.dragPos)
+#             self.dragPos = event.globalPos()
+#             event.accept()
             
 class User_Window(QMainWindow, Ui_User_WIndow):
     def __init__(self):
@@ -61,16 +53,7 @@ class User_Window(QMainWindow, Ui_User_WIndow):
         
     def close_event(self):
         self.hide()
-        ppt.show()
-        
-    def mousePressEvent(self, event):                                 # +
-        self.dragPos = event.globalPos()
-        
-    def mouseMoveEvent(self, event):                                  # !!!
-        if event.buttons() == QtCore.Qt.LeftButton:
-            self.move(self.pos() + event.globalPos() - self.dragPos)
-            self.dragPos = event.globalPos()
-            event.accept()
+        ppt.showFullScreen()
             
 class PPT_Window(QMainWindow, Ui_MainWindow2):
     def __init__(self):
@@ -79,20 +62,15 @@ class PPT_Window(QMainWindow, Ui_MainWindow2):
         self.setupUi(self)
         self.stackedWidget.setCurrentWidget(self.home)
         
-    def mousePressEvent(self, event):                                 # +
-        self.dragPos = event.globalPos()
-        
-    def mouseMoveEvent(self, event):                                  # !!!
-        if event.buttons() == QtCore.Qt.LeftButton:
-            self.move(self.pos() + event.globalPos() - self.dragPos)
-            self.dragPos = event.globalPos()
-            event.accept()
+    def keyPressEvent(self, event):
+        if event.key() == QtCore.Qt.Key_Escape:
+            self.close()
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     myWindow = MyWindow()
-    admin_window = Administrator_Window()
+    # admin_window = Administrator_Window()
     user_window = User_Window()
     ppt = PPT_Window()
     myWindow.show()
